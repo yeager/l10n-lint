85da32016b709aa5bb2ccbcf666a4bb8eb396b8a
BOSSe <BOSSe@danielnylander.se>
2026-02-17 11:44:00 +0100

    v1.15.9: Update Swedish translation (100%)

--- l10n_lint.py
+++ l10n_lint.py
@@ -30,7 +30,7 @@ from pathlib import Path
 from typing import Generator, Optional
 from urllib.parse import urlparse
 
-__version__ = "1.15.8"
+__version__ = "1.15.9"
 
 # Translation setup
 DOMAIN = "l10n-lint"

cba80f4953a747ed26e54314cf08f94873206044
Daniel Nylander <daniel@danielnylander.se>
2026-03-07 18:35:24 +0100

    v1.18.0: typo detection, decimal-separator, HTML tag fix

--- l10n_lint.py
+++ l10n_lint.py
@@ -30,7 +30,7 @@ from pathlib import Path
 from typing import Generator, Optional
 from urllib.parse import urlparse
 
-__version__ = "1.17.2"
+__version__ = "1.18.0"
 
 # Translation setup
 DOMAIN = "l10n-lint"
